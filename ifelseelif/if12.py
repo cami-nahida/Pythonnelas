04bf5afd0da02a5b726907ea73626c6f0fb071a9
cami-nahida <camilevravadelli@gmail.com>
2026-07-24 14:48:03 -0300

    Add files via upload

---
+++ ifelseelif/if12.py
@@ -0,0 +1,8 @@
+nome = "Amai"
+
+#o len()conta quantas letras uma palavra tem.
+
+if len(nome)> 6:
+    print("Voce tem um nome grande")
+else:
+    print("Seu nome é curto")
---
+++ ifelseelif/if13.py
@@ -0,0 +1,7 @@
+idade = int(input("Digite a sua idade"))
+if idade < 12:
+    print("Voce é uma criança🍭")
+elif idade >= 12 and idade < 18:
+    print("Voce é um adolecente")
+else:
+    print("Voce é um adulto!")
